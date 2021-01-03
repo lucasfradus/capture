2b804ba41779810b83e7703ad81fd49049fedf6c
lucasfradus <lucas.fradusco@gmail.com>
2021-01-03 17:31:28 -0300

    Initial commit

---
+++ node-course/react-starter/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+function App() {
+return(
+<div className="tx"></div>
+)
+export default App;
---
+++ node-course/react-starter/src/TestComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TestComponent(){
+    return(
+        <div>
+            <h2>Welcome</h2>
+                <div>hola</div>
+            <h3>React Compnenet</h3>
+        </div>
+    );
+}   
+
+export default TestComponent;
