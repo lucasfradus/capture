9565139dc9823881f57f18d25b1b2dffe3334c94
lucasfradus <lucas.fradusco@gmail.com>
2021-01-03 20:48:12 -0300

    2. About Us

--- react/capture/src/App.js
+++ react/capture/src/App.js
@@ -1,11 +1,11 @@
+import React from 'react';
 
+import AboutUs from './pages/aboutUs';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Hello React
-      </h1>
+      <AboutUs />
     </div>
   );
 }
---
+++ react/capture/src/pages/aboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AboutUsSection from '../components/AboutUs';
+
+const aboutUs = () => {
+    return (
+        <div>   
+            <AboutUsSection/>
+        </div>
+            
+    )
+}
+
+export default aboutUs;
