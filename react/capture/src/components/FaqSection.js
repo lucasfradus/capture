2d32e351177d949af85b1a743243d1fe96e53810
lucasfradus <lucas.fradusco@gmail.com>
2021-01-03 21:14:59 -0300

    FAQ section

---
+++ react/capture/src/components/FaqSection.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+
+const FaqSection = () =>{
+    return (
+        <div className="faqSection">
+            <h1>FAQ</h1>
+            <div className="question">
+                <h2>How do I start?</h2>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Voluptate neque iusto quisquam blanditiis cum facilis itaque aspernatur molestiae mollitia accusamus.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h2>How do I start?</h2>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Voluptate neque iusto quisquam blanditiis cum facilis itaque aspernatur molestiae mollitia accusamus.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h2>How do I start?</h2>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Voluptate neque iusto quisquam blanditiis cum facilis itaque aspernatur molestiae mollitia accusamus.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h2>How do I start?</h2>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Voluptate neque iusto quisquam blanditiis cum facilis itaque aspernatur molestiae mollitia accusamus.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default FaqSection;
--- react/capture/src/pages/aboutUs.js
+++ react/capture/src/pages/aboutUs.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import AboutUsSection from '../components/AboutUs';
 import Services from '../components/ServicesSection';
-
+import FaqSection from '../components/FaqSection';
 
 const aboutUs = () => {
     return (
         <>   
             <AboutUsSection/>
             <Services/>
+            <FaqSection/>
         </>
             
     )
